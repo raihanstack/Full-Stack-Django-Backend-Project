b11e2b79cc698ce3e9d6652bbb108d93aa18b73a
Mostafa Raihan <raihan.invite@gmail.com>
2026-04-24 09:15:03 +0600

    site user create

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -137,14 +137,12 @@ CORS_ALLOWED_ORIGINS = [
 
 #rest framework settings
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-    ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
         'rest_framework.filters.SearchFilter',
     ),
 }
+
+ACCOUNT_SESSION_REMEMBER = True
+
+AUTH_USER_MODEL = 'user_api.SiteUser'
