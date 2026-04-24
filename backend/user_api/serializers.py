b1dd41de1449384dab0cc8ff200b587a7e4bfa68
Mostafa Raihan <raihan.invite@gmail.com>
2026-04-24 09:43:38 +0600

    urls setup

--- backend/user_api/serializers.py
+++ backend/user_api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from rest_framework.authentication.models import Token
+from rest_framework.authtoken.models import Token
 from .models import SiteUser
 
 class UserSerializer(serializers.ModelSerializer):
---
+++ backend/user_api/urls.py
@@ -0,0 +1,10 @@
+from rest_framework import routers
+from .views import UserViewSet
+from django.urls import re_path, include
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
+urlpatterns = [
+    re_path(r'^', include(router.urls)),
+]
