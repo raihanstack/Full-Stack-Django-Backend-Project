8f8929c82402a54a70ad88ab484d6d0778d3d98d
Mostafa Raihan <raihan.invite@gmail.com>
2026-04-24 10:56:35 +0600

    views.py

--- backend/product_api/views.py
+++ backend/product_api/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import filters
 
-# Create your views here.
+from .models import Product
+from .serializers import ProductSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name', 'description']
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+    def perform_update(self, serializer):
+        serializer.save()
+    
+    def perform_destroy(self, instance):
+        instance.delete()
+
+    
