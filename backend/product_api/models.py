550d8113b6818c19be2cd74d6207e928b9fac9ef
Mostafa Raihan <raihan.invite@gmail.com>
2026-04-24 10:46:19 +0600

    product _ model

--- backend/product_api/models.py
+++ backend/product_api/models.py
@@ -1,3 +1,23 @@
+from datetime import datetime
+import os
 from django.db import models
 
-# Create your models here.
+def upload_loaction(instance, filename):
+    filebase, extension = os.path.splitext(filename)
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+    return 'product_images/%s/%s.%s' % (
+        instance.name,
+        timestamp,
+        extension.replace('.', '')
+    )
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    photo = models.ImageField(upload_to=upload_loaction, blank=True)
+
+    def __str__(self):
+        return self.name
